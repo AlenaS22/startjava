2f1d11730e38bfbba1f6bc87ce0f7d4955f68d29
AlenaS22 <alena_w93@mail.ru>
2021-02-27 22:41:58 +0300

    HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main (String[] args) {
+        byte core = 2;
+        short proc = 4;
+        int RAM = 8;
+        long verOfBoard = 1;
+        float frequency = 2.3f;
+        double verOfSMBIOS = 3.1d;
+        char type = 'i';
+        boolean security = true;
+        System.out.println(core + "\n" + proc + "\n" + RAM + "\n" +
+            verOfBoard + "\n" + frequency + "\n" + verOfSMBIOS +
+            "\n" + type + "\n" + security);
+    }
+}
