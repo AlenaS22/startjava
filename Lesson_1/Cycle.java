58dd528fbbdc49c204ccba5f4642bb0c18f6185f
AlenaS22 <alena_w93@mail.ru>
2021-03-09 22:24:38 +0300

    HW_3

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,23 @@
+public class Cycle {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+        System.out.println("---");
+        
+        int i = 6;
+        while(i >= -6) {
+            System.out.println(i);
+            i -= 2;
+        }
+        System.out.println("---");
+
+        i = 11;
+        int sum = 0;
+        do {
+            sum += i;
+            i += 2;
+        } while(i < 20);
+        System.out.println(sum);
+    }
+}
