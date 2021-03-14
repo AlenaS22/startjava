d239eedb8fcf12ecc27508a4d63e742ec4d2b71b
AlenaS22 <alena_w93@mail.ru>
2021-03-14 14:24:47 +0300

    HW_4

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int number1 = 23;
+        int number2  = 7;
+        double result = 0;
+        char operation = '^';
+
+        if(operation == '+') {
+            result = number1 + number2;
+        } else if(operation == '-') {
+            result = number1 - number2;
+        } else if(operation == '*') {
+            result = number1 * number2;
+        } else if(operation == '/') {
+            result = 1.0 * number1 / number2;
+        } else if(operation == '^') {
+            int pow = 5;
+            result = number1;
+            for(int i = 0; i < pow - 1; i++) {
+                result *= number1;
+            }
+        } else {
+            result = number1 % number2;
+        }
+        
+        System.out.println(result);
+    }
+}
+
