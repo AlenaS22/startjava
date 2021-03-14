f747caef887856716ec4ea312b70c00ed0ebbea0
AlenaS22 <alena_w93@mail.ru>
2021-03-14 15:50:13 +0300

    HW_5

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,20 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int valueOfComputer = 15;
+        int valueOfGamer = 0;
+
+        while(valueOfGamer != valueOfComputer) {
+            if(valueOfGamer < valueOfComputer) {
+                System.out.println("Введенное вами число " + valueOfGamer +
+                " меньше того, что загадал компьютер");
+                valueOfGamer+=2;
+            } else {
+                System.out.println("Введенное вами число " + valueOfGamer +
+                " больше того, что загадал компьютер");
+                valueOfGamer--;
+            }
+        }
+        System.out.println("Вы угадали! Число, загаданное компьютером, равняется " +
+        valueOfComputer + "!");
+    }
+}
