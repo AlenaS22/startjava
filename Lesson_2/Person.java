ef721e67c8f5e04d3b1585f8fb9c5d96eceb9bec
AlenaS22 <alena_w93@mail.ru>
2021-03-24 20:58:05 +0300

    HW_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char gender = 'ж';
+    String name = "Алена";
+    float height = 1.7f;
+    int weight = 70;
+    int age = 27;
+
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void speak() {
+
+    }
+
+    void learnJava() {
+        
+    }
+}
