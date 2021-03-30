750629d9afbfc27a238a231e59925fcfaa81f1a7
AlenaS22 <alena_w93@mail.ru>
2021-03-30 14:51:34 +0300

    HW_4

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,70 @@
+public class Calculator {
+    private int number1;
+    private int number2;
+    private double result;
+    private char operation;
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+    
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public double getResult() {
+        return result;
+    }
+    
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+    
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public void calculation() {
+        //обнуление result для случаев, когда введен некорректный знак
+        result = 0;
+        switch(operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = 1.0 * number1 / number2;
+                break;
+            case '^':
+                result = number1;
+                for(int i = 0; i < number2 - 1; i++) {
+                result *= number1;
+                }
+                break;
+            case '%':
+                result = number1 % number2;
+                break;
+            default:
+                System.out.println("Введен некорректный знак!");
+                break;
+        }
+    }
+}
+
