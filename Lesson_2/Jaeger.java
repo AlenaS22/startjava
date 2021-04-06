aa698fd8daa5bff018d7954dd071dd98a1f32fa7
AlenaS22 <alena_w93@mail.ru>
2021-04-06 20:08:51 +0300

    HW_5

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -7,6 +7,17 @@ public class Jaeger {
     private int speed;
     private int strength;
     private int armor;
+    // конструктор
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
 
     public String getModelName() {
         return modelName;
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger gipsyDanger = new Jaeger();
-        Jaeger chernoAlpha = new Jaeger();
+        Jaeger gipsyDanger = new Jaeger("Gipsy Danger", "Mark-3", "USA", 79.25f, 1.98f, 7, 8, 6);
+        Jaeger chernoAlpha = new Jaeger("Cherno Alpha", "Mark-1", "Russia", 85.34f, 2.412f, 3, 10, 10);
         Scanner scanner = new Scanner(System.in);
         // Jaeger 1
-        gipsyDanger.setModelName("Gipsy Danger");
+        /* gipsyDanger.setModelName("Gipsy Danger");
         gipsyDanger.setMark("Mark-3");
         gipsyDanger.setOrigin("USA");
         gipsyDanger.setHeight(79.25f);
@@ -22,7 +22,7 @@ public class JaegerTest {
         chernoAlpha.setWeight(2.412f);
         chernoAlpha.setSpeed(3);
         chernoAlpha.setStrength(10);
-        chernoAlpha.setArmor(10);
+        chernoAlpha.setArmor(10); */
         // Jaeger 1 характеристики
         System.out.println("Характеристики Jaeger 1:");
         System.out.println("model name: " + gipsyDanger.getModelName());
