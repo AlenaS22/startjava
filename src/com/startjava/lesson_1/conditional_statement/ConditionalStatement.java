0348e60bd6df2c5a6d6c4aa044cb44c3a6325a04
AlenaS22 <alena_w93@mail.ru>
2021-04-14 20:48:15 +0300

    Lesson_3

--- src/com/startjava/lesson_1/conditional_statement/ConditionalStatement.java
+++ src/com/startjava/lesson_1/conditional_statement/ConditionalStatement.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson1.conditional_statement;
+
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 25;
--- src/com/startjava/lesson_1/unicode/Unicode.java
+++ src/com/startjava/lesson_1/unicode/Unicode.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson1.unicode;
+
 public class Unicode {
     public static void main(String[] args) {
         for(char symbol = 33; symbol < 127; symbol++) {
